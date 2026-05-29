fd2893a57cde5a84c21e90aae70343f8d346beac
Abdoulie Bah <bahablie03@gmail.com>
2026-05-29 19:36:01 +0000

    Update API URL to production backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://utg-attendance-v2-backend.onrender.com'
 });
 
 // Attach token to every request
