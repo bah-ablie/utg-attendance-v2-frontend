b6c634bf4dd1e5f0c312c86335f56183227c3afa
Abdoulie Bah <bahablie03@gmail.com>
2026-05-30 20:03:27 +0000

    Fix API base URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://utg-attendance-v2-backend.onrender.com'
+  baseURL: 'https://utg-attendance-v2-backend.onrender.com/api'
 });
 
 // Attach token to every request
